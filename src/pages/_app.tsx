070b0aeb22633fc68e42686220beb10938101f99
TAAHCOINBLOCKCHAIN <tahcoinblockchain@gmail.com>
2024-09-29 21:47:14 +0600

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
+import RootLayout from '../layout'; // Adjust the path as necessary
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/index.tsx.bak
@@ -0,0 +1,10 @@
+import { YourComponent } from "../components/YourComponent";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Welcome to Your AI Assistant</h1>
+      <YourComponent />
+    </div>
+  );
+}
